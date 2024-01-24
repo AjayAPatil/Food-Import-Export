687d5691085b8b668fcc6f346a5e33416d413d7e
Ajay Patil <ajaypatil9765@gmail.com>
2024-01-25 00:11:25 +0530

    Initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Food.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Food.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult LoginPage()
+        {
+            return View("Login");
+        }
+        public ResponseModel Login(UserModel data)
+        {
+            ResponseModel response = new();
+            if (string.IsNullOrEmpty(data?.UserName))
+            {
+                response.Status = "error";
+                response.Message = "Username not found";
+            }
+            else if(data.Password == "123")
+            {
+                response.Status = "success";
+                response.Message = "Login Successfully";
+                response.Data = new UserModel
+                {
+                    UserId = 001,
+                    UserName = data.UserName,
+                    Password = data.Password,
+                    Role = "Admin"
+                };
+            }
+            else
+            {
+                response.Status = "fail";
+                response.Message = "Invalid Password";
+                response.Data = data;
+            }
+            return response;
+        }
+    }
+}
---
+++ Models/ResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Food.Models
+{
+    public class ResponseModel
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+    }
+}
